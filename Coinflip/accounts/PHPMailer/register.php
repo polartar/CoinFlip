08ffd34e427989c08487ab84baf7b75a9799d114
talentdev2020 <polaris.center@outlook.com>
2020-12-03 08:41:36 -0700

    Remove Password

--- Coinflip/accounts/PHPMailer/register.php
+++ Coinflip/accounts/PHPMailer/register.php
@@ -43,8 +43,8 @@
 					$mail->isSMTP();                                            // Set mailer to use SMTP
 					$mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 					$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-					$mail->Username   = 'vgocheapnoreply@gmail.com';                     // SMTP username
-					$mail->Password   = 'z7wWXYEL24LV5Vs3';                               // SMTP password
+					$mail->Username   = 'Your email';                     // SMTP username
+					$mail->Password   = 'Email Password';                               // SMTP password
 					$mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
 					$mail->Port       = 465;                                    // TCP port to connect to
 
